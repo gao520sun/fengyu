c5b860e4ed868a9b1913db17d751051dbd6ced9d
gaozhonglei <gaozhonglei@yuehuikang.com>
2020-06-26 14:26:00 +0800

    f

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ docker build . -t fengyu:web
 docker save fengyu:web -o fengyu
 docker ps -a|grep "fengyu-web" | awk '{print $1}' | xargs docker stop
 docker ps -a|grep "fengyu-web" | awk '{print $1}' | xargs docker rm
-docker run -d --name=fengyu-web -p 5000:80 fengyu:web
+docker run -d --name=fengyu-web -p 5000:5000 fengyu:web
 docker rmi $(docker images -a|grep none|awk '{print $3}')
